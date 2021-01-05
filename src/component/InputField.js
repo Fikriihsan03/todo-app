0720361d7bfd9ea6db2fc4d1ca8b8199b9b5f2fc
fikri <mfikriihsan03@gmail.com>
2021-01-05 10:31:07 +0700

    fix plus className

--- src/component/InputField.js
+++ src/component/InputField.js
@@ -1,24 +1,32 @@
-import React ,{ useState}from 'react';
+import React, { useState } from "react";
 
-const InputField = ({add,total}) => {
-    const [currentValue,setValue] = useState('')
-    const addTask = () =>{
-        const newTask = {
-            id:Date.now(),
-            massage :currentValue,
-            isDone:false
-        }
-        add([...total,newTask])
-        setValue('')
-    }
-    
-    return(
-        <>
-        <div className="row d-flex justify-content-center">
-        <input value={currentValue}type="text" onChange ={(event => setValue(event.target.value))} />
-        <button disabled={currentValue===''}className ="btn btn-primary fas fa-plus" onClick={()=>addTask()} ></button>
-        </div>
-        </>
-    )
-}
+const InputField = ({ add, total }) => {
+  const [currentValue, setValue] = useState("");
+  const addTask = () => {
+    const newTask = {
+      id: Date.now(),
+      massage: currentValue,
+      isDone: false,
+    };
+    add([...total, newTask]);
+    setValue("");
+  };
+
+  return (
+    <>
+      <div className="row d-flex justify-content-center">
+        <input
+          value={currentValue}
+          type="text"
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button
+          disabled={currentValue === ""}
+          className="btn-primary fas fa-plus"
+          onClick={() => addTask()}
+        ></button>
+      </div>
+    </>
+  );
+};
 export default InputField;
